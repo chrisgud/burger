dc0640cb05aa7c2678a097158a69669cf3364649
Chris Gudmundsson <chrisgud85@gmail.com>
2018-10-13 13:09:52 -0500

    Updated connection info

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,11 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const connection
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'chris',
