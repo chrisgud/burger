667ed1ee387c957b79a91432e48dd2252be04f7a
Chris Gudmundsson <chrisgud85@gmail.com>
2018-10-13 13:19:06 -0500

    Corrected typo

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,22 @@
 const mysql = require('mysql');
 
-const connection
+let connection;
 
-if(process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'chris',
-    password: 'mypass',
-    database: 'burger_db',
-});
+        host: 'localhost',
+        port: 3306,
+        user: 'chris',
+        password: 'mypass',
+        database: 'burger_db',
+    });
+}
 
 connection.connect((err) => {
     if (err) throw err;
     console.log(`Connected.  ID=${connection.threadId}`);
-})
+});
 
 module.exports = connection;
